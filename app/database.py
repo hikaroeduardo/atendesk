20d04e6524a9cdd587fd1ef66d3eda94956b65d4
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2025-05-16 12:19:52 -0300

    feat: create session for database

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, SQLModel, create_engine, Session
 from .models.model import User, Ticket, Customer
 
 sqlite_file_name = "database.db"
@@ -6,6 +6,7 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url)
 
+session = Session(engine)
 
 def create_all_tables():
     SQLModel.metadata.create_all(engine)
